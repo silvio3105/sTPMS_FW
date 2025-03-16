9ec72646805ca888b5480909d5eef1b7708e1cec
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2025-03-16 17:20:26 +0100

    Added test code #1.

--- Application/Inc/Main.hpp
+++ Application/Inc/Main.hpp
@@ -28,6 +28,7 @@
 // ----- INCLUDE FILES
 #include			"AppConfig.hpp"
 #include 			"TPMS1.hpp"
+#include			"sDebug.hpp"
 
 #include			<stdint.h>
 
@@ -36,9 +37,37 @@
 
 
 // ----- MACRO FUNCTIONS
+/**
+ * @brief Calculate constant string length.
+ * 
+ * @param _input Input C-string.
+ * 
+ * @return Length of \c _input C-string.
+ */
+#define __CONST_STR_LEN(_input) \
+	(sizeof(_input) - 1)
 
+/**
+ * @brief Calculate array members.
+ * 
+ * @param _input Input array.
+ * 
+ * @return Number of members in array.
+ */
+#define __ARRAY_LEN(_input) \
+	(sizeof(_input) / sizeof(_input[0]))
 
 // ----- ENUMS
+/**
+ * @brief Enum class with return values.
+ * 
+ */
+enum class Return_t : uint8_t
+{
+	OK = 0, /**< @brief OK return value. */
+	NOK, /**< @brief Not OK return value. */
+	Timeout /**< @brief Timeout return value. */
+};
 
 
 // ----- TYPEDEFS
@@ -57,6 +86,26 @@
 
 
 // ----- EXTERNS
+
+
+// ----- DEBUG
+#ifdef DEBUG_VERBOSE
+DEBUG_ENABLE_VERBOSE();
+#else // DEBUG_VERBOSE
+DEBUG_DISABLE_VERBOSE();
+#endif // DEBUG_VERBOSE
+
+#ifdef DEBUG_INFO
+DEBUG_ENABLE_INFO();
+#else // DEBUG_INFO
+DEBUG_DISABLE_INFO();
+#endif // DEBUG_INFO
+
+#ifdef DEBUG_ERROR
+DEBUG_ENABLE_ERROR();
+#else // DEBUG_ERROR
+DEBUG_DISABLE_ERROR();
+#endif // DEBUG_ERROR
 
 
 
--- Application/Main.cpp
+++ Application/Main.cpp
@@ -25,55 +25,57 @@
 // ----- INCLUDE FILES
 #include			"Main.hpp"
 #include			"SEGGER_RTT.h"
-#include			"sDebug.hpp"
+#include			"BLE.hpp"
 
-#ifdef USING_RTOS
-#include			"cmsis_os2.h"
-#endif // RTOS
+#include 			"nrf_log.h"
+#include 			"nrf_log_ctrl.h"
+#include 			"nrf_log_default_backends.h"
 
 #include			<stdint.h>
 #include 			<stdio.h>
 #include			<string.h>
 
 
-// ----- DEBUG
-DEBUG_ENABLE_VERBOSE();
-DEBUG_ENABLE_INFO();
-DEBUG_ENABLE_ERROR();
-
-
-// ----- DEFINES
-
-
-// ----- MACRO FUNCTIONS
-
-
-// ----- ENUMS
-
-
-// ----- TYPEDEFS
-
-
-// ----- STRUCTS
-
-
-// ----- CLASSES	
-
-
-// ----- VARIABLES
-
-
-// ----- STATIC FUNCTION DECLARATIONS
-
-
 // ----- FUNCTION DEFINITIONS
 int main(void)
 {
-	_PRINT("Test\n");
+	#ifdef DEBUG
+	NRF_LOG_INIT(NULL);
+	NRF_LOG_DEFAULT_BACKENDS_INIT();
+	#endif // DEBUG
 
+	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
+	NRF_CLOCK->TASKS_HFCLKSTART = 1;
+	_PRINT("Wait\n");
+	while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
+	_PRINT("Started\n");
+	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;	
+
+	for (uint32_t i = 0; i < 0xFFFFFF; i++)
+	{
+		(void)i;
+	}
+
+	if (BLE::init() != Return_t::OK)
+	{
+		_PRINT_ERROR("BLE init fail\n");
+	}
+	else
+	{
+		_PRINT_INFO("BLE init OK\n");
+	}
+
+	_PRINT("Start\n");
+
+	uint8_t cnt = 0;
 	while (1)
 	{
-		
+		BLE::advertise(++cnt);
+		_PRINT("Loop\n");
+		for (uint32_t i = 0; i < 0xFFFFFF; i++)
+		{
+			(void)i;
+		}
 	}
 }
 
