883c1b69b85910d78f001eeee25742da565aad56
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2025-03-17 01:04:55 +0100

    Added stuff to app config #1.

--- Config/AppConfig.hpp
+++ Config/AppConfig.hpp
@@ -36,6 +36,7 @@
  * @{
  */
 
+
 // ----- CONFIGURATION
 /**
  * @brief Application configuration namespace.
@@ -43,10 +44,25 @@
  */
 namespace AppConfig
 {
+	// ----- ENUMS
+	/**
+	 * @brief Enum class for hardware IDs.
+	 * 
+	 */
+	enum class Hardware_t : uint8_t
+	{
+		sTPMS1 = 1, /**< @brief Hardware ID for sTPMS1. */
+	};
+
+
+	// ----- VARIABLES
 	static constexpr char deviceName[] = "sTPMS1"; /**< @brief Bluetooth device name. */
 	static constexpr int8_t advTXPower = 4; /**< @brief Bluetooth TX power for advertising role(-40, -20, -16, -12, -8, -4, 0, or 4dBm). */
 	static constexpr uint16_t advPeriod = 160; // Advertising period in ms with resolution of 0.625ms.
 	static constexpr uint8_t advCount = 1; /**< @brief Number of advertisment events before stopping advertise. */
+	static constexpr uint8_t bleTag = 1; /**< @brief Bluetooth connection tag. */
+	static constexpr Hardware_t hwID = Hardware_t::sTPMS1; /**< @brief sTPMS hardware ID. */
+	static constexpr uint8_t rttChannel = 0; /**< @brief RTT channel ID for debug output. */
 };
 
 
