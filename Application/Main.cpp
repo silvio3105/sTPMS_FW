86e8d6f3fe31a7adfe00a8c40726aa17a78805cf
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2025-03-17 01:05:46 +0100

    Added sTPMS data example #1.

--- Application/Main.cpp
+++ Application/Main.cpp
@@ -26,6 +26,7 @@
 #include			"Main.hpp"
 #include			"SEGGER_RTT.h"
 #include			"BLE.hpp"
+#include			"System.hpp"
 
 #include 			"nrf_log.h"
 #include 			"nrf_log_ctrl.h"
@@ -36,7 +37,40 @@
 #include			<string.h>
 
 
+// ----- STRUCTS
+/**
+ * @brief sTPMS data packet.
+ * 
+ */
+struct sTPMSData_s
+{
+	uint16_t pressure; /**< @brief Measured pressure in mbar. */
+	int16_t temperature; /**< @brief Measured temperature in centi degrees celsius. */
+	uint16_t uptime; /**< @brief Device uptime in hours. */
+	System::Error_t errorCode; /**< @brief Last error code. See \ref System::Error_t. */
+	uint8_t voltage; /**< @brief Battery voltage in centivolts. Offset: 200cV. */
+	uint8_t fwVer[3]; /**< @brief sTPMS firmware version - major, minor, build. */
+	const AppConfig::Hardware_t hwID = AppConfig::hwID; /**< @brief sTPMS hardware ID. See \ref AppConfig::Hardware_t. */
+	System::ResetReason_t rstReason; /**< @brief Reset reason. See \ref System::ResetReason_t. */
+	uint8_t rstCount; /**< @brief Reset counter. */
+};
+
+
+// ----- VARIABLES
+static sTPMSData_s data; /**< @brief Sensor data to advertise. */
+
+
+// ----- STATIC FUNCTION DECLARATIONS
+static void initTPMSData(void);
+static void updateTPMSData(void);
+
+
 // ----- FUNCTION DEFINITIONS
+/**
+ * @brief Application entry point.
+ * 
+ * @return No return value.
+ */
 int main(void)
 {
 	#ifdef DEBUG
@@ -44,17 +78,17 @@ int main(void)
 	NRF_LOG_DEFAULT_BACKENDS_INIT();
 	#endif // DEBUG
 
-	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;
-	NRF_CLOCK->TASKS_HFCLKSTART = 1;
-	_PRINT("Wait\n");
-	while (NRF_CLOCK->EVENTS_HFCLKSTARTED == 0);
-	_PRINT("Started\n");
-	NRF_CLOCK->EVENTS_HFCLKSTARTED = 0;	
+	_PRINTF("%d bytes\n", sizeof(data));
 
-	for (uint32_t i = 0; i < 0xFFFFFF; i++)
+	// Init system
+	if (System::init() != Return_t::OK)
 	{
-		(void)i;
+		_PRINT_ERROR("System init fail\n");
+		// SOON: Add reset
 	}
+
+	// Init sTPMS data struct
+	initTPMSData();
 
 	if (BLE::init() != Return_t::OK)
 	{
@@ -67,11 +101,10 @@ int main(void)
 
 	_PRINT("Start\n");
 
-	uint8_t cnt = 0;
 	while (1)
 	{
-		BLE::advertise(++cnt);
-		_PRINT("Loop\n");
+		updateTPMSData();
+		BLE::advertise(&data, sizeof(data));
 		for (uint32_t i = 0; i < 0xFFFFFF; i++)
 		{
 			(void)i;
@@ -79,14 +112,61 @@ int main(void)
 	}
 }
 
+/**
+ * @brief Debug output handler.
+ * 
+ * @param string Pointer to string to output.
+ * @param len Length of \c string.
+ * 
+ * @return No return value.
+ */
 void sDebug::out(const char* string, const uint16_t len)
 {
-	SEGGER_RTT_Write(0, string, len);
+	SEGGER_RTT_Write(AppConfig::rttChannel, string, len);
 }
 
 
 // ----- STATIC FUNCTION DEFINITIONS
+/**
+ * @brief Init advertise data struct.
+ * 
+ * @return No return value.
+ */
+static void initTPMSData(void)
+{
+	// SOON: Set real values
 
+	// Set firmware version
+	data.fwVer[0] = 1;
+	data.fwVer[1] = 2;
+	data.fwVer[2] = 3;
+
+	// Set reset stuff
+	data.rstReason = System::ResetReason_t::Powerup;
+	data.rstCount = 1;
+}
+
+/**
+ * @brief Update advertise data struct.
+ * 
+ * @return No return value.
+ */
+static void updateTPMSData(void)
+{
+	// SOON: Set real values
+
+	// Update errors
+	data.errorCode = System::Error_t::None;
+
+	// Set measured values
+	data.pressure = 1013;
+	data.temperature = 2552;
+	data.voltage = 120;
+
+	// Set uptime in hours
+	data.uptime++;
+	//data.uptime = 1;
+}
 
 
 // END WITH NEW LINE
