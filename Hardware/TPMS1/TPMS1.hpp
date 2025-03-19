2bf45b2af5105c81fa04e52762b0ac42dbf569ef
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2025-03-19 22:12:37 +0100

    Added hardware config #1.

--- Hardware/TPMS1/TPMS1.hpp
+++ Hardware/TPMS1/TPMS1.hpp
@@ -36,6 +36,21 @@
 
  
 // ----- NAMESPACES
+/**
+ * @brief Namespace with hardware configuration.
+ * 
+ */
+namespace Hardware
+{
+	static constexpr uint8_t ptsSCLPort = 0; /**< @brief Pressure and temperature sensor I2C SCL GPIO port. */
+	static constexpr uint8_t ptsSCLPin = 7; /**< @brief Pressure and temperature sensor I2C SCL GPIO pin. */
+	static constexpr uint8_t ptsSDAPort = 0; /**< @brief Pressure and temperature sensor I2C SDA GPIO port. */
+	static constexpr uint8_t ptsSDAPin = 8; /**< @brief Pressure and temperature sensor I2C SDA GPIO pin. */
+	static constexpr uint8_t ptsSelectPort = 0; /**< @brief Pressure and temperature sensor select GPIO port. */
+	static constexpr uint8_t ptsSelectPin = 9; /**< @brief Pressure and temperature sensor select GPIO pin. */
+	static constexpr uint8_t ledPort = 0; /**< @brief LED GPIO port. */
+	static constexpr uint8_t ledPin = 2; /**< @brief LED GPIO pin. */
+};
 
 
 /** @} */
