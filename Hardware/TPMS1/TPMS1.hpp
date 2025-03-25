d2ab2c6e201ebf67c77a285468781534f89ee49a
silvio3105 <104866334+silvio3105@users.noreply.github.com>
2025-03-26 00:30:44 +0100

    Fixed wrong pin number for PTS select.

--- Hardware/TPMS1/TPMS1.hpp
+++ Hardware/TPMS1/TPMS1.hpp
@@ -50,7 +50,7 @@ namespace Hardware
 	static constexpr uint8_t ptsSDAPort = 0; /**< @brief Pressure and temperature sensor I2C SDA GPIO port. */
 	static constexpr uint8_t ptsSDAPin = 8; /**< @brief Pressure and temperature sensor I2C SDA GPIO pin. */
 	static constexpr uint8_t ptsSelectPort = 0; /**< @brief Pressure and temperature sensor select GPIO port. */
-	static constexpr uint8_t ptsSelectPin = 9; /**< @brief Pressure and temperature sensor select GPIO pin. */
+	static constexpr uint8_t ptsSelectPin = 5; /**< @brief Pressure and temperature sensor select GPIO pin. */
 	static constexpr uint8_t ledPort = 0; /**< @brief LED GPIO port. */
 	static constexpr uint8_t ledPin = 2; /**< @brief LED GPIO pin. */
 };
